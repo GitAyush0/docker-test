6963d764614674958a3fde6b8f3a2c1b459aa9a3
GitAyush0 <ayushtiwari3636@gmail.com>
2025-06-23 14:57:40 +0530

    demo-app-v1

---
+++ src/main/java/com/docker/demo/Docker/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.docker.demo.Docker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("message")
+    public String getHello(){
+        return("Hello This From Docker Setup");
+    }
+}
